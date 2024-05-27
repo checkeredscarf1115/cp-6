3d5f97de4445e36ec867e40fcc0dc10b81b59032
checkeredscarf1115 <possragr@gmail.com>
2024-05-27 16:37:31 +0800

    Добавлено сообщение успешности бронирования мест

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,16 @@ setDate("input_date");
 window.showSelectTime = showSelectTime;
 showSelectTime("select_time", "select_route", "input_date");
 window.setSelectedSeats = setSelectedSeats;
+window.onSearchClick = onSearchClick;
+window.showReserveButton = showReserveButton;
+window.onReserveClick = onReserveClick;
 
 let seats = [];
+let msg = document.getElementById("message");
+let selectRoute = document.getElementById("select_route");
+let selectBusStop = document.getElementById("select_bus_stop");
+let inputDate = document.getElementById("input_date");
+let selectTime = document.getElementById("select_time");
 
 function setSelectedSeats(div, cell) {
     let index = seats.indexOf(cell.innerHTML);
@@ -23,4 +31,27 @@ function setSelectedSeats(div, cell) {
     }
 
     div.innerHTML = "Выбрано: " + seats.toString();
+}
+
+function showReserveButton(id) {
+    let elem = document.getElementById(id);
+    if (seats.length > 0 && elem.classList.contains("invisible")) {
+        elem.classList.remove("invisible");
+        elem.classList.add("visible");
+    } else if (seats.length <= 0) {
+        elem.classList.remove("visible");
+        elem.classList.add("invisible");
+    }
+}
+
+function onSearchClick(id) {
+    let search_res = document.getElementById(id);
+    search_res.classList.remove("d-none");
+}
+
+function onReserveClick() {
+    msg.innerHTML = `Места ${seats.toString()} были успешно забронированы для маршрута ${
+        selectRoute.value
+    } на ${inputDate.value} на время ${selectTime.value}`;
+    msg.classList.remove("d-none");
 }
--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.authorzed')
 
 @section('inside-content')
-<div class="row">
+<div class="row seat-available mx-1 py-4 px-2 mb-4 d-none" id="message">
+    Были успешно забронированы
+</div>
+
+<div class="row mx-2">
     <select class="col-2 mx-2" id="select_route" onchange="showSelectTime('select_time', 'select_route', 'input_date')"  >
         <option value="1" selected>1</option>
         <option value="2">2</option>
     </select>   
 
-    <select class="col-2 mx-2">
+    <select class="col-2 mx-2" id="select_bus_stop">
         <option>Остановка 1</option>
         <option>Остановка 2</option>
         <option>Остановка 3</option>
@@ -22,41 +26,47 @@
     
     <select class="col-2 mx-2" id="select_time"></select>
 
-    <div class="col-1 mx-2 btn btn-primary">Поиск</div>
+    <div class="col-1 mx-2 btn btn-primary" onclick="onSearchClick('search_result')">Поиск</div>
 </div>
 
-<div class="pt-2" id="selected">Выбрано: </div>
+<div class="d-none" id="search_result">
+    <div class="pt-2" id="selected">Выбрано: </div>
 
-<hr class="border-4"/>
+    <hr class="border-4"/>
 
-<div class="row">
-    <div class="container py-5">
-        @for ($i = 0; $i < 10; $i++)
-            <div class="row justify-content-center">
-                @if ($i == 9)
-                    @for ($j = 1; $j <= 5; $j++)
-                        @if (in_array($j + $i * 4, $seats_reserved))
-                            <div class="col-auto text-center seat-reserved m-1 text-black">{{ $j + $i * 4 }}</div>
-                        @else
-                            <a href="#" onclick="setSelectedSeats(selected, this)" class="col-auto text-center seat-available m-1 text-black">{{ $j + $i * 4 }}</a>
-                        @endif
-                    @endfor
+    <button class="btn btn-primary invisible" id="reserve_button" onclick="onReserveClick()">Забронировать</button>
 
-                @else
-                
-                    @for ($j = 1; $j <= 4; $j++)
-                        @if ($j == 3)
-                                <div class="col-auto invisible m-1">0</div> 
-                        @endif
-                        @if (in_array($j + $i * 4, $seats_reserved))
-                            <div class="col-auto text-center seat-reserved m-1 text-black">{{ $j + $i * 4 }}</div>
-                        @else
-                            <a href="#" onclick="setSelectedSeats(selected, this)" class="col-auto text-center seat-available m-1 text-black">{{ $j + $i * 4 }}</a>
-                        @endif
-                    @endfor
-                @endif
-            </div>
-        @endfor
+    <div class="row">
+        <div class="container py-2">
+            @for ($i = 0; $i < 10; $i++)
+                <div class="row justify-content-center">
+                    @if ($i == 9)
+                        @for ($j = 1; $j <= 5; $j++)
+                            @if (in_array($j + $i * 4, $seats_reserved))
+                                <div class="col-auto text-center seat-reserved m-1 text-black">{{ $j + $i * 4 }}</div>
+                            @else
+                                <a href="#" onclick="setSelectedSeats(selected, this); showReserveButton('reserve_button', this)" class="col-auto text-center seat-available m-1 text-black">{{ $j + $i * 4 }}</a>
+                            @endif
+                        @endfor
+
+                    @else
+                    
+                        @for ($j = 1; $j <= 4; $j++)
+                            @if ($j == 3)
+                                    <div class="col-auto invisible m-1">0</div> 
+                            @endif
+                            @if (in_array($j + $i * 4, $seats_reserved))
+                                <div class="col-auto text-center seat-reserved m-1 text-black">{{ $j + $i * 4 }}</div>
+                            @else
+                                <a href="#" onclick="setSelectedSeats(selected, this); showReserveButton('reserve_button', this)" class="col-auto text-center seat-available m-1 text-black">{{ $j + $i * 4 }}</a>
+                            @endif
+                        @endfor
+                    @endif
+                </div>
+            @endfor
+        </div>
     </div>
 </div>
+
+
 @endsection
