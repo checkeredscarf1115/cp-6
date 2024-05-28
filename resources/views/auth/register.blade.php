1175ec6b13924f0b791e3c5787372f3f3e75f413
checkeredscarf1115 <possragr@gmail.com>
2024-05-28 20:20:18 +0800

    коррекция цвета и расположения формы регистрации

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Регистрация') }}</div>
+            <div class="card border-0">
+                <div class="card-header theme-color">{{ __('Регистрация') }}</div>
 
-                <div class="card-body">
+                <div class="card-body theme-color-light-gray">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
